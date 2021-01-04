8e2d37825e3a9c39cce06ad461423262985ec918
ramadanhagag96 <76247208+ramadanhagag96@users.noreply.github.com>
2021-01-04 14:24:36 +0200

    Add files via upload

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 /* Global Variables */
-const apiKey = "&appid=18b745bec5cda7ff912d6b67432b1acc";
+const apiKey = "&appid=18b745bec5cda7ff912d6b67432b1acc&units=imperial";
 const zip= document.getElementById('zip');
 const feelings = document.getElementById('feelings');
 
@@ -60,14 +60,15 @@ async function posttoserver(data){
 }
     // update UI
 async function updateUI(){
-     const request = await fetch('http://localhost:3030/all');
+     let request = await fetch('http://localhost:3030/all');
   try{
       
-      const all = await request.json();
+      const all1 = await request.json();
+       console.log(all1);
          //update entry values
-      document.getElementById('date').innerHTML = 'data is :'+all.date;
-      document.getElementById('temp').innerHTML = 'temperature is :'+all.temp;
-      document.getElementById('content').innerHTML = 'your feelings is :'+all.content;
+      document.getElementById('date').innerHTML = 'data is :'+all1.date;
+      document.getElementById('temp').innerHTML = 'temperature is :'+all1.temp;
+      document.getElementById('content').innerHTML = 'your feelings is :'+all1.content;
       console.log("UI updated seccussfully");
       
 
